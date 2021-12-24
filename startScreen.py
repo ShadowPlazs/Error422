e3fb8b800757b9c7350780878b69d814c0b89788
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2021-12-24 12:14:19 +0800

    Update and rename startScreen.js to startScreen.py

---
+++ startScreen.py
@@ -0,0 +1,14 @@
+import minecraft
+from minecraft
+  import start 
+ import server.py
+  
+def on_error:
+  print("An unexpected error has eccoured. Please try again.")
+  refresh('Server.py')
+
+def on_display1('Acti'):
+  mainColor('')
+  secoColor('')
+  refresh('Server.py')
+  
